9b7f79c3a53d39a390560a86fa907cc4cecc012c
kairat y <k_y_2022@mail.ru>
2022-04-10 15:38:58 +0600

    Added js files for registration flow;

--- web/js/login.js
+++ web/js/login.js
@@ -11,8 +11,6 @@ $(document).ready(function () {
             $("#login-form").submit();
         }
     });
-
-
 });
 
 function hasErrors() {
---
+++ web/js/registration/org_main_info.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $("#form-submit-button").click(function () {
+        console.log(" click submit button ")
+       
+    });
+});
