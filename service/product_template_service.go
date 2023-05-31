b6355f25542cdfbd87cda407c0ed9273a50ef893
Kairat Yussupov <k.yussupov@infin-it.kz>
2023-05-31 11:27:14 +0600

    product template service;

---
+++ service/product_template_service.go
@@ -0,0 +1,27 @@
+package service
+
+import "example.com/m/v2/repository"
+
+type ProductTemplateService struct {
+	ProductTemplateRepository *repository.ProductTemplateRepository
+}
+
+func (p ProductTemplateService) GetById() {
+
+}
+
+func (p ProductTemplateService) Create() {
+
+}
+
+func (p ProductTemplateService) Update() {
+
+}
+
+func (p ProductTemplateService) Delete() {
+
+}
+
+func (p ProductTemplateService) FindAll() {
+
+}
